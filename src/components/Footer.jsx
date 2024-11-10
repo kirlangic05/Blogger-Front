a3f4b538257e81fffd8c675f7caf3be22eb3e057
kirlangic05 <furkanmelihkirlangic@gmail.com>
2024-11-10 14:04:56 +0300

    Kırlangıç mühendislik yazıp deploy denemesi yapılıyor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='footer'>@copyright Kırlangıç engineering</div>
+    <div className='footer'>@copyright Kırlangıç mühendislik</div>
   )
 }
