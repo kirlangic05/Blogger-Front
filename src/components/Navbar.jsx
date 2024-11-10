2d9410a413b094197909fcdef2e2851863d271b8
kirlangic05 <furkanmelihkirlangic@gmail.com>
2024-11-10 13:58:30 +0300

    Kırlangıç Bloggg yazıp deploy denemesi yapılıyor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Kırlangıç BloggG</a>
+          <a className="navbar-brand" href="#">Kırlangıç Bloggg</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
